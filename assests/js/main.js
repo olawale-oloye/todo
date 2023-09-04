6745f86d55d13e9b6b8c42d5b22169d4f6d6a9d6
olawale-oloye <olawaleoloye@gmail.com>
2023-09-05 06:08:00 +1000

    update to JS

--- assests/js/main.js
+++ assests/js/main.js
@@ -5,6 +5,11 @@ const formCont = document.querySelector("form");
 const inputField = document.querySelector("input");
 const todoItemContainer = document.querySelector(".todo-item-container");
 const todayDate = document.querySelector(".today-date");
+const completedTask = document.getElementsByClassName("completed");
+const completedTaskArr = [];
+const activeBtn = document.querySelector(".active-btn");
+const active = document.getElementsByClassName("active");
+const activeTaskArr = [];
 
 // let todos = JSON.parse(localStorage.getItem("todos"));
 // let todoItemArray = [];
@@ -38,10 +43,13 @@ formCont.addEventListener("submit", function (e) {
     todoItemArray.push(todoObject);
     localStorage.setItem("todos", JSON.stringify(todoItemArray));
 
-    todoItemContainer.innerHTML += `<div class="todo-item1">
-                                      <i class="ri-circle-line"></i>
-                                      <p>${inputField.value}</p>
-                                    </div>`;
+    todoItemContainer.innerHTML += `<div class = "todo-item-style">
+                                        <div class="todo-item1">
+                                          <i class="ri-circle-line"></i>
+                                          <p class = "todo-text active">${inputField.value}</p>
+                                        </div>
+                                        <i class = "delete"> X </i>
+                                     </div>`;
     // todoItemContainer.style.display = "color: white";
 
     inputField.value = "";
@@ -52,10 +60,13 @@ formCont.addEventListener("submit", function (e) {
 function displayTodoItem() {
   todoItemArray.forEach((todo) => {
     let divElement = document.createElement("div");
-    divElement.innerHTML = `<i class="ri-circle-line"></i>
-                              <p>${todo.todoText}</p>
-                           `;
-    divElement.classList.add("todo-item1");
+    divElement.innerHTML = ` <div class="todo-item1">
+                                <i class="ri-circle-line"></i>
+                                <p class = "todo-text active">${todo.todoText}</p>
+                              </div>
+                              <i class = "delete" onclick="deleteToDo(${todo.todoId})"> X </i>`;
+
+    divElement.classList.add("todo-item-style");
     todoItemContainer.appendChild(divElement);
   });
   // console.log(todos);
@@ -76,10 +87,42 @@ document.querySelector(".clear-all").addEventListener("click", () => {
 });
 
 todoItemContainer.addEventListener("click", (e) => {
-  // console.log(e.target);
-  if (e.target.classList.contains("todo-item1")) {
-    e.target.addEventListener("click", () => {
-      e.target.style.textDecoration = "line-through";
-    });
+  // console.log(e.target.parentElement);
+  if (e.target.classList.contains("todo-text")) {
+    // e.target.style.textDecoration = "line-through";
+    e.target.classList.toggle("completed");
+    e.target.classList.toggle("active");
+    completedTaskArr.push(e.target.textContent);
+    if (e.target.classList.contains("active")) {
+      console.log(e.target);
+      activeTaskArr.push(e.target.textContent);
+    }
+  }
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
   }
 });
+document.querySelector(".completed-btn").addEventListener("click", function () {
+  completedTaskArr.forEach((todo) => {
+    let divElement = document.createElement("div");
+    divElement.innerHTML = `<div class="todo-item1">
+                                <i class="ri-circle-line"></i>
+                                <p class = "todo-text">${todo}</p>
+                           </div>
+                           <i class = "delete"> X </i>`;
+    divElement.classList.add("todo-item-style");
+    todoItemContainer.appendChild(divElement);
+  });
+  console.log(completedTaskArr);
+});
+
+activeBtn.addEventListener("click", function () {
+  console.log(activeTaskArr);
+});
+
+function deleteToDo(todoId) {
+  let todoIndex = todoItemArray.findIndex((todo) => todo.todoId === todoId);
+  todoItemArray.splice(todoIndex, 1);
+  localStorage.setItem("todos", JSON.stringify(todoItemArray));
+  console.log(todoIndex);
+}
